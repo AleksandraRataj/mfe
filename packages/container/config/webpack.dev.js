4650ac6be877ea535faba7f0b21c9afd630cff88
Aleksandra Rataj <arataj@guidewire.com>
2024-03-17 15:36:27 +0100

    chore: created container and marketing prod webpack configs

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 const packageJson = require('../package.json');
@@ -14,9 +14,6 @@ const devConfig = {
         },
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        }),
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
@@ -24,7 +21,6 @@ const devConfig = {
             },
             shared: packageJson.dependencies,
         }),
-
     ]
 }
 
