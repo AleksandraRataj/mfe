96803bc0ec3bb679b63fe73a77bcd2adff971c56
Aleksandra Rataj <arataj@guidewire.com>
2024-03-17 14:09:12 +0100

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const mount = (element) => {
+    console.log("marketing");
+
+
+    ReactDOM.render(
+        <App/>,
+        element
+    )
+}
+
+if(process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#marketing-dev-root');
+
+    if(devRoot){
+        mount(devRoot);
+    }
+}
+
+export {mount};
